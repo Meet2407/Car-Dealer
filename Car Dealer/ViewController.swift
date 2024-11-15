fb4be24e5dd3e9941a4c4624feb877082a942180
Translate App <meetkapadiya65@gmail.com>
2024-11-15 10:43:19 +0530

    Initial Commit

---
+++ Car Dealer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Car Dealer
+//
+//  Created by Meet Kapadiya on 15/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
